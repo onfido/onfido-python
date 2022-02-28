659af1f44e787c39adefb5194e3cc5f11109fec9
Phoebe Baxter <phoebe.baxter@onfido.com>
2022-02-28 16:48:54 +0000

    Update to v3.3

--- tests/test_documents.py
+++ tests/test_documents.py
@@ -9,7 +9,7 @@ fake_uuid = "58a9c6d2-8661-4dbd-96dc-b9b9d344a7ce"
 
 
 def test_upload_document(requests_mock):
-    mock_upload = requests_mock.post("https://api.eu.onfido.com/v3.2/documents/", json=[])
+    mock_upload = requests_mock.post("https://api.eu.onfido.com/v3.3/documents/", json=[])
 
     request_body = {"applicant_id": fake_uuid,
                     "document_type": "driving_licence"}
@@ -27,17 +27,17 @@ def test_upload_document_missing_params():
         api.document.upload(sample_file=string_io)
 
 def test_find_document(requests_mock):
-    mock_find = requests_mock.get(f"https://api.eu.onfido.com/v3.2/documents/{fake_uuid}", json=[])
+    mock_find = requests_mock.get(f"https://api.eu.onfido.com/v3.3/documents/{fake_uuid}", json=[])
     api.document.find(fake_uuid)
     assert mock_find.called is True
 
 def test_list_documents(requests_mock):
-    mock_list = requests_mock.get(f"https://api.eu.onfido.com/v3.2/documents?applicant_id={fake_uuid}", json=[])
+    mock_list = requests_mock.get(f"https://api.eu.onfido.com/v3.3/documents?applicant_id={fake_uuid}", json=[])
     api.document.all(fake_uuid)
     assert mock_list.called is True
 
 def test_download_document(requests_mock):
-    mock_download = requests_mock.get(f"https://api.eu.onfido.com/v3.2/documents/{fake_uuid}/download", text="FAKE IMAGE BINARY", headers={"Content-type": "image/png"})
+    mock_download = requests_mock.get(f"https://api.eu.onfido.com/v3.3/documents/{fake_uuid}/download", text="FAKE IMAGE BINARY", headers={"Content-type": "image/png"})
     onfido_download = api.document.download(fake_uuid)
     assert mock_download.called is True
     assert onfido_download.content_type == "image/png"
