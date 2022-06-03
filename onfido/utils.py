7a28b47749cde6cbec95050fbaeae4abd468138b
Davide Vacca <davide.vacca@onfido.com>
2022-06-03 10:59:06 +0200

    Support nested structures in upload request

--- onfido/utils.py
+++ onfido/utils.py
@@ -2,20 +2,21 @@
 def form_data_converter(data):
     """ Flat nested dictionary and stringify booleans for form-data """
 
+    def _converter(data, result, prefix=''):
+        if isinstance(data, dict):
+            for k, v in data.items():
+                pref = '{}[{}]'.format(prefix,k) if prefix else k
+                _converter(v, result, pref)
+        elif isinstance(data, bool):
+            result[prefix] = str(data).lower()
+        else:
+            result[prefix] = data
+
     # If no dict or booleans among values, no conversion needed
-    if not any(type(item) in (dict, bool) for item in data.values()):
+    if not any(isinstance(item, dict) or isinstance (item, bool) for item in data.values()):
         return data
 
     result = {}
-
-    # Perform needed conversion for each
-    for k, v in data.items():
-        if isinstance(v, dict):
-            for child_k, child_v in v.items():
-                result['{}[{}]'.format(k, child_k)] = child_v
-        elif isinstance(v, bool):
-            result[k] = str(v).lower()
-        else:
-            result[k] = v
+    _converter(data, result)
 
     return result
