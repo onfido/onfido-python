6b54515133d0970f04d2b9deb690b49b96016b85
kuznik27 <51229906+kuznik27@users.noreply.github.com>
2022-12-29 15:03:21 +0300

    Update watchlist_monitors.py

--- onfido/resources/watchlist_monitors.py
+++ onfido/resources/watchlist_monitors.py
@@ -5,12 +5,12 @@ class WatchlistMonitors(Resource):
     def create(self, request_body:dict):
         return self._post("watchlist_monitors/", **request_body)
 
-    def find(self, check_id:str):
+    def find(self, monitor_id:str):
         return self._get(f"watchlist_monitors/{monitor_id}")
 
     def all(self, applicant_id:str):
         payload = {"applicant_id": applicant_id}
         return self._get("watchlist_monitors/", payload=payload)
   
-    def delete(self, applicant_id:str):
+    def delete(self, monitor_id:str):
         self._delete_request(f"watchlist_monitors/{monitor_id}")
