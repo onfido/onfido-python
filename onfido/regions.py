4ff37d5e7df095c9846c467df1562323caa814e5
Sergio den Boer <sergio.boer@onfido.com>
2023-01-19 14:07:47 +0000

    Point to new version 3.6

--- onfido/regions.py
+++ onfido/regions.py
@@ -2,9 +2,9 @@ from enum import Enum
 
 
 class Region(Enum):
-    EU = "https://api.eu.onfido.com/v3.5/"
-    US = "https://api.us.onfido.com/v3.5/"
-    CA = "https://api.ca.onfido.com/v3.5/"
+    EU = "https://api.eu.onfido.com/v3.6/"
+    US = "https://api.us.onfido.com/v3.6/"
+    CA = "https://api.ca.onfido.com/v3.6/"
 
     @property
     def region_url(self):
