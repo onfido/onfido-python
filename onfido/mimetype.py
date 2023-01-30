ba5e512e19fcddbb24cd459578b149bb017979de
rbarran-yescapa <123385938+rbarran-yescapa@users.noreply.github.com>
2023-01-30 16:21:36 +0100

    Handle filenames with a uppercase file extension. (#35)

--- onfido/mimetype.py
+++ onfido/mimetype.py
@@ -1,6 +1,5 @@
 import mimetypes
-import os
 
 def mimetype_from_name(filename):
-    name, extension = os.path.splitext(filename)
-    return mimetypes.types_map[extension]
+    mimetype, _ = mimetypes.guess_type(filename)
+    return mimetype
--- tests/test_mimetypes.py
+++ tests/test_mimetypes.py
@@ -7,3 +7,7 @@ def test_mimetypes():
 
 def test_secondary_mimetypes():
     assert mimetype_from_name("filename.jpeg") == "image/jpeg"
+
+def test_uppercase():
+    """Uppercase file extensions are handled."""
+    assert mimetype_from_name("filename.JPG") == "image/jpeg"
