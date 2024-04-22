b781eb3ff589675ade4858eca2c3f0ef60706126
Davide Vacca <davide.vacca@onfido.com>
2024-04-22 12:25:46 +0200

    Implement basic end to end tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,15 @@
+import onfido
+from os import environ
 import pytest
-from aioresponses import aioresponses
+
 
 @pytest.fixture
-def m():
-    with aioresponses() as mock:
-        yield mock
+def onfido_api():
+    configuration = onfido.Configuration(
+      api_token=environ['ONFIDO_API_TOKEN'],
+      region=onfido.configuration.Region.EU,
+    )
+    configuration.debug = True
+
+    with onfido.ApiClient(configuration) as api_client:
+        yield onfido.DefaultApi(api_client)
--- tests/test_webhook_verification.py
+++ tests/test_webhook_verification.py
@@ -1,44 +1,40 @@
-import onfido
-from onfido.regions import Region
-from onfido.webhook_event_verifier import WebhookEventVerifier
-from onfido.exceptions import OnfidoInvalidSignatureError
+from onfido.webhook_event_verifier import WebhookEventVerifier, OnfidoInvalidSignatureError
+from onfido import WebhookEvent, WebhookEventPayload, WebhookEventPayloadObject
+
 import pytest
 
-api = onfido.Api("<AN_API_TOKEN>", region=Region.EU)
+raw_event = ("{\"payload\":{\"resource_type\":\"check\",\"action\":\"check.completed\","
+             "\"object\":{\"id\":\"f2302f45-227d-413d-ad61-09ec077a086a\",\"status\":\"complete\","
+             "\"completed_at_iso8601\":\"2024-04-04T09:21:21Z\","
+             "\"href\":\"https://api.onfido.com/v3.6/checks/f2302f45-227d-413d-ad61-09ec077a086a\"}}}")
 
-raw_event = (
-    '{"payload":'
-    '{"resource_type":"check","action":"check.completed","object":'
-    '{"id":"check-123",'
-    '"status":"complete","completed_at_iso8601":"2020-01-01T00:00:00Z",'
-    '"href":"https://api.onfido.com/v3/checks/check-123"}}}'
+secret_token = "wU99mE6jJ7nXOLFwZ0tJymM1lpI15pZh"
+
+expected_event = WebhookEvent(
+    payload=WebhookEventPayload(
+        action="check.completed",
+        resource_type="check",
+        object=WebhookEventPayloadObject(
+            id='f2302f45-227d-413d-ad61-09ec077a086a',
+            href='https://api.onfido.com/v3.6/checks/f2302f45-227d-413d-ad61-09ec077a086a',
+            status="complete",
+            completed_at_iso8601='2024-04-04T09:21:21Z'
+        )
+    )
 )
-
-secret_token = "_ABC123abc123ABC123abc123ABC123_"
-
-expected_event = {
-    "action": "check.completed",
-    "resource_type": "check",
-    "object": {
-        "id": "check-123",
-        "href": "https://api.onfido.com/v3/checks/check-123",
-        "status": "complete",
-        "completed_at_iso8601": "2020-01-01T00:00:00Z"
-    }
-}
 
 verifier = WebhookEventVerifier(secret_token)
 
 
 def test_webhook_verification():
-    signature = "a0082d7481f9f0a2907583dbe1f344d6d4c0d9989df2fd804f98479f60cd760e"
+    signature = "77ebc3e418f26be6eebb47f7ebe551321de26734fc273961e075fc9ab163d9c7"
 
     event = verifier.read_payload(raw_event, signature)
     assert event == expected_event
 
 
 def test_webhook_verification_invalid_signature():
-    signature = "b0082d7481f9f0a2907583dbe1f344d6d4c0d9989df2fd804f98479f60cd760e"
+    signature = "77ebc3e418f26be6eebb47f7ebe551321de26734fc273961e075fc9ab163d9c8"
 
     with pytest.raises(OnfidoInvalidSignatureError):
         verifier.read_payload(raw_event, signature)
