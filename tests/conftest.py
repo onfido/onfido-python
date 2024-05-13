e892b4f8f91e2d2d1a8a50f19f478171fac3dc71
Sofia Gomes <sofia.gomes@onfido.com>
2024-05-13 16:37:02 +0100

    test: add clean up after tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,32 @@ from os import environ
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def onfido_api():
     configuration = onfido.Configuration(
-      api_token=environ['ONFIDO_API_TOKEN'],
-      region=onfido.configuration.Region.EU,
+        api_token=environ["ONFIDO_API_TOKEN"],
+        region=onfido.configuration.Region.EU,
     )
     configuration.debug = True
 
     with onfido.ApiClient(configuration) as api_client:
         yield onfido.DefaultApi(api_client)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def data_clean_up(onfido_api):
+    sample_applicant_id = environ["ONFIDO_SAMPLE_APPLICANT_ID"]
+
+    applicants = onfido_api.list_applicants(
+        page=1, per_page=100, include_deleted=False
+    ).applicants
+    for applicant in applicants:
+        if applicant.id != sample_applicant_id:
+            try:
+                onfido_api.delete_applicant(applicant.id)
+            except onfido.ApiException as e:
+                # Just ignore any failure during cleanup
+                pass
 
 
 def create_applicant(onfido_api, applicant_builder=None):
